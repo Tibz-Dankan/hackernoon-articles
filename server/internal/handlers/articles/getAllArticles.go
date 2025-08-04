524b0aa5407f6d2b99312020983459a07961e689
Tibz-Dankan <tibesigwadankan@gmail.com>
2025-08-04 12:50:38 +0300

    refactor: improve article fetch functionality

--- server/internal/handlers/articles/getAllArticles.go
+++ server/internal/handlers/articles/getAllArticles.go
@@ -1,27 +1,52 @@
 package articles
 
 import (
+	"log"
+	"strconv"
+	"time"
+
 	"github.com/Tibz-Dankan/hackernoon-articles/internal/models"
 	"github.com/Tibz-Dankan/hackernoon-articles/internal/pkg"
 	"github.com/gofiber/fiber/v2"
 )
 
-// To significantly improve the article
 var GetAllArticles = func(c *fiber.Ctx) error {
 	articles := models.Article{}
 	limitParam := c.Query("limit")
-	cursorParam := c.Query("cursor")
+	articleIDCursorParam := c.Query("articleIDCursor")
+	dateCursorParam := c.Query("dateCursor")
+	offsetParam := c.Query("offset")
 
 	limit, err := pkg.ValidateQueryLimit(limitParam)
 	if err != nil {
 		return fiber.NewError(fiber.StatusBadRequest, err.Error())
 	}
 
-	if cursorParam == "" {
-		cursorParam = ""
+	if articleIDCursorParam == "" {
+		articleIDCursorParam = ""
 	}
 
-	allArticles, err := articles.FindAllByPostedAt(limit, cursorParam)
+	var parsedDateCursorParam time.Time
+	var offset int
+
+	if dateCursorParam != "" {
+		parsedDateCursorParam, err := time.Parse(time.RFC3339, dateCursorParam)
+		if err != nil {
+			return fiber.NewError(fiber.StatusBadRequest, "Invalid offset format! Must be an ISO 8601 string.")
+		}
+		log.Printf("parsedDateCursorParam: %v\n", parsedDateCursorParam)
+
+	}
+
+	if offsetParam != "" {
+		offset, err = strconv.Atoi(offsetParam)
+		if err != nil {
+			log.Println("Error converting offsetParam to an integer:", err)
+		}
+		log.Println(offset)
+	}
+
+	allArticles, count, err := articles.FindAllByPostedAt(int(limit), articleIDCursorParam, parsedDateCursorParam, offset)
 	if err != nil {
 		return fiber.NewError(fiber.StatusBadRequest, err.Error())
 	}
@@ -34,6 +59,8 @@ var GetAllArticles = func(c *fiber.Ctx) error {
 	pagination := map[string]interface{}{
 		"limit":      limit,
 		"prevCursor": prevCursor,
+		"count":      count,
+		"offset":     offset,
 	}
 
 	response := fiber.Map{
