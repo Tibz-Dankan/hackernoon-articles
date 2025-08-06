2649b53e3f4a6f2c26c38de60744c5033f55eb3f
Tibz-Dankan <tibesigwadankan@gmail.com>
2025-08-07 01:28:54 +0300

    feat: add loading more items

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Layout } from "./Layout";
 import type { TArticle } from "../types/articles";
-import { RefreshCw, RotateCcw } from "lucide-react";
+import { ArrowRight, RefreshCw, RotateCcw } from "lucide-react";
 import { useQuery } from "@tanstack/react-query";
 import { article } from "../API/articles";
 import { useSearchParams } from "react-router";
@@ -11,24 +11,19 @@ import { useFormik } from "formik";
 import * as Yup from "yup";
 import { Button } from "./Button";
 import { SquareBlocks } from "./SquareBlocks";
+import type { Loader } from "../types/loader";
+import type { Pagination } from "../types/pagination";
 
 export const Home: React.FC = () => {
-  const [searchParams] = useSearchParams();
-
-  // const [searchParams, setSearchParams] = useSearchParams();
-
-  // const updateParams = () => {
-  //   // Set new parameters (replaces existing ones)
-  //   setSearchParams({
-  //     userId: '123',
-  //     category: 'electronics'
-  //   });
-  // };
+  const [articles, setArticles] = useState<TArticle["article"][]>([]);
+  const [pagination, setPagination] = useState<Pagination>();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [loader, setLoader] = useState<Loader>("INITIAL");
   const articleIDCursor = searchParams.get("aIDCursor");
   const dateCursor = searchParams.get("dCursor");
   const offset = searchParams.get("offset");
 
-  const { isPending, isError, data, error } = useQuery({
+  const { isPending, data } = useQuery({
     queryKey: [`articles-${articleIDCursor}-${dateCursor}-${offset}`],
     queryFn: () => {
       return article.getAll({
@@ -51,7 +46,11 @@ export const Home: React.FC = () => {
     }),
     onSubmit: async (values, helpers) => {
       try {
-        // submit functionality here
+        setLoader(() => "DATE_CURSOR");
+        setSearchParams({
+          dCursor: new Date(values.timeTravelBitcoin).toISOString(),
+          aIDCursor: "",
+        });
       } catch (error) {
         helpers.setStatus({ success: false });
         helpers.setSubmitting(false);
@@ -59,27 +58,45 @@ export const Home: React.FC = () => {
     },
   });
 
-  const articles: TArticle["article"][] = data?.data ?? [];
-  if (isPending) {
+  // const articles: TArticle["article"][] = data?.data ?? [];
+  // const pagination: Pagination = data?.pagination ?? {};
+  const isInitialLoader = loader === "INITIAL";
+  const isArticleIDCursorLoader = loader === "ARTICLE_ID_CURSOR";
+  const isDateCursorLoader = loader === "DATE_CURSOR";
+
+  const triggerLoadMoreArticles = () => {
+    setLoader(() => "ARTICLE_ID_CURSOR");
+    setSearchParams({
+      dCursor: "",
+      aIDCursor: pagination!.prevCursor,
+    });
+  };
+
+  useEffect(() => {
+    const updateArticles = () => {
+      setArticles(() => data?.data ?? []);
+      setPagination(() => data?.pagination ?? {});
+    };
+    updateArticles();
+  }, [data]);
+
+  if (isPending && isInitialLoader) {
     return (
       <Layout>
-        <div className="text-green-700">Loading</div>
+        <div
+          className="text-gray-50 flex-1 w-full h-full flex 
+           items-center justify-center gap-2"
+        >
+          <RefreshCw className="animate-spin" size={24} />
+          <span>Loading..</span>
+        </div>
       </Layout>
     );
   }
 
-  // if (isError ) {
-  //   return (
-  //     <Layout>
-  //     <div className="text-green-700">{error.message}</div>
-  //   </Layout>
-  //   );
-  // }
-
   return (
     <Layout>
       <div className="flex flex-col gap-8">
-        {/* memepool square blocks here */}
         <form
           onSubmit={formik.handleSubmit}
           className="flex flex-col items-center gap-2 text-gray-200 self-center"
@@ -107,8 +124,8 @@ export const Home: React.FC = () => {
                       <span>Submit</span>
                     </div>
                   )}
-                  {isPending && (
-                    <div className="flex items-center justify-center gap-2 ">
+                  {isPending && isDateCursorLoader && (
+                    <div className="flex items-center justify-center gap-2">
                       <RefreshCw className="animate-spin" size={24} />
                       <span>Submitting</span>
                     </div>
@@ -127,6 +144,31 @@ export const Home: React.FC = () => {
               <ArticleCard article={article} />
             </div>
           ))}
+        </div>
+        <div className="flex items-center justify-center text-(--clr-primary)">
+          <Button
+            label={
+              <>
+                {!isPending && (
+                  <div className="flex items-center justify-center gap-2">
+                    <span>Next</span>
+                    <ArrowRight size={20} />
+                  </div>
+                )}
+                {isPending && isArticleIDCursorLoader && (
+                  <div className="flex items-center justify-center gap-2">
+                    <RefreshCw className="animate-spin" size={24} />
+                    <span>Loading...</span>
+                  </div>
+                )}
+              </>
+            }
+            type={"button"}
+            disabled={isPending}
+            className="min-w-40 bg-(--clr-background) border-[1px]
+             border-[rgba(73,80,87,0.6)]"
+            onClick={() => triggerLoadMoreArticles()}
+          />
         </div>
         <div>
           <SquareBlocks />
---
+++ client/src/types/loader.ts
@@ -0,0 +1,1 @@
+export type Loader = "INITIAL" | "ARTICLE_ID_CURSOR" | "DATE_CURSOR";
